bb0844ea74cad9f88878ebd265f9304ec5de467c
Aryan Patel <patelaryan7751@gmail.com>
2023-05-31 17:45:10 +0530

    Level 1 assignment files

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,16 +1,51 @@
 import TaskCard from "./TaskCard";
 function App() {
   return (
-    <div>
-      <div>
-        <h1>Pending</h1>
-        <TaskCard title="Build the website with static content" />
-        <TaskCard title="Add blog" />
+    <div className="mx-4 px-24">
+      <div className="my-4">
+        <h1 className="text-3xl my-2 font-extrabold">Smarter Tasks</h1>
+        <p className="text-gray-600 font-semibold text-xl">
+          <b>Project:</b> Graduation Final Year Project (Revamp College Website)
+        </p>
       </div>
-      <div>
-        <h1>Done</h1>
-        <TaskCard title="Deign the mockup" />
-        <TaskCard title="Get approval from principal" />
+      <div className="grid grid-cols-2 gap-2">
+        <div className="border-2 border-gray-400 rounded-lg p-2">
+          <h1 className="text-center text-2xl text-gray-600 font-bold">
+            Pending
+          </h1>
+          <TaskCard
+            title="Build the website with static content"
+            dueDate="10th April"
+            assigneeName="Rohit S"
+            completed={false}
+          />
+          <TaskCard
+            title="Add a blog"
+            dueDate="22nd March"
+            assigneeName="Rohit M"
+            completed={false}
+          />
+          <div className="m-3">
+            <button className="border-4 border-slate-100 bg-gray-300 p-1 w-full text-left font-semibold text-xl">
+              + New task
+            </button>
+          </div>
+        </div>
+        <div className="border-2 border-gray-400 rounded-lg p-2">
+          <h1 className="text-center text-2xl text-gray-600 font-bold">Done</h1>
+          <TaskCard
+            title="Deign the mockup"
+            completedAtDate="10th April"
+            assigneeName="Rohit M"
+            completed={true}
+          />
+          <TaskCard
+            title="Get approval from principal"
+            completedAtDate="20th April"
+            assigneeName="Ajay S"
+            completed={true}
+          />
+        </div>
       </div>
     </div>
   );
--- hello-react/src/TaskCard.js
+++ hello-react/src/TaskCard.js
@@ -3,10 +3,19 @@ import "./TaskCard.css";
 const TaskCard = (props) => {
   console.log(props);
   return (
-    <div className="TaskItem">
-      <h2 className="text-xl font-bold">{props.title}</h2>
-      <p>Completed on: due date...</p>
-      <p>Assignee: name...</p>
+    <div className="border-4 border-gray-400 p-3 m-3">
+      <h2 className="text-xl font-bold mb-2">{props.title}</h2>
+      {props.completed ? (
+        <p className="text-gray-600 font-semibold">
+          Completed on: {props?.completedAtDate}
+        </p>
+      ) : (
+        <p className="text-gray-600  font-semibold">Due on: {props?.dueDate}</p>
+      )}
+
+      <p className="text-gray-600  font-semibold">
+        Assignee: {props.assigneeName}
+      </p>
     </div>
   );
 };
